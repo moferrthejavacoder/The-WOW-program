7366916cae24457aab857e0efa31667172f58047
moferrthejavacoder <moferryt@gmail.com>
2024-10-08 22:43:48 -0700

    https://github.com/moferrthejavacoder/The-WOW-program

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿
+Console.Title = "My great wow program";
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine("(type command for commands)WOWWWW");
+string Response = Console.ReadLine();
+if (Response == "wowlot")
+{
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+    Console.WriteLine("WOWWWW");
+
+}
+if (Response == "wowagain")
+{
+    Console.WriteLine("WOWWWW");
+
+}
+if (Response == "longwow")
+{
+   
+    Console.WindowHeight = 43;
+
+}
+if (Response == "command")
+{
+    Console.WriteLine("wowagain: puts another wow.\nwowlot: too many wows.\nlongwow:makes the window long");
+    
+}
+Console.ReadKey();
